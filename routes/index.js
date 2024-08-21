8568d8624211bed5c3c43daf321dc2d530a3eade
Cirene1997 <gjoaquinreynoso@gmail.com>
2024-08-21 12:57:58 -0300

    add the new function

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Nueva ruta que envía el texto desde el backend
+router.get('/mensaje-backend', (req, res) => {
+  res.send('Hola, soy el backend de tu proyecto');
+});
+
 module.exports = router;
